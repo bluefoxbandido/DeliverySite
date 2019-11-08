9a4d033c70b47cec6b00d7e28d98037b19625a4f
bluefoxbandido <adamthewonderboy@gmail.com>
2019-11-08 11:54:17 -0600

    Adam's Initial

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+const deliveryAddress = (streetAddress, city, state, zip) => {
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+}
+
+const paymentInformation = (cardType, cardholderName, cardNumber, exp, cvv) => {
+    this.cardType = cardType,
+    this.cardholderName = cardholderName;
+    this. cardNumber = cardNumber;
+    this.exp = exp;
+    this.cvv = cvv;
+}
+
+const cart = (product)
